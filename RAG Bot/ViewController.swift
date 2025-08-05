8f470b5bb04a1a7c6c80751d1e4ca80334e0bd3c
Srihan Vege <vegesrihan@gmail.com>
2025-08-05 17:06:24 -0500

    Initial Commit

---
+++ RAG Bot/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RAG Bot
+//
+//  Created by Srihan Vege on 8/5/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
